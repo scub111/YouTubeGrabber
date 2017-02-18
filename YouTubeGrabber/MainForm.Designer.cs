f84eb9610e45db924ef9588956a78eb2058afef9
Biskub Konstantin <scub111@mail.ru>
2017-02-18 15:22:13 +0300

    2017.02.18 15:21 - Added HTML parser (AngleSharp)

---
+++ YouTubeGrabber/MainForm.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace YouTubeGrabber
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.memoEdit1 = new DevExpress.XtraEditors.MemoEdit();
+            this.btnAnalyse = new DevExpress.XtraEditors.SimpleButton();
+            this.tdInputUrl = new DevExpress.XtraEditors.TextEdit();
+            this.layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.sprbProgress = new System.Windows.Forms.ToolStripProgressBar();
+            this.slblCaption = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+            this.layoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.memoEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tdInputUrl.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // layoutControl1
+            // 
+            this.layoutControl1.Controls.Add(this.memoEdit1);
+            this.layoutControl1.Controls.Add(this.btnAnalyse);
+            this.layoutControl1.Controls.Add(this.tdInputUrl);
+            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutControl1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.Root = this.layoutControlGroup1;
+            this.layoutControl1.Size = new System.Drawing.Size(644, 455);
+            this.layoutControl1.TabIndex = 0;
+            this.layoutControl1.Text = "layoutControl1";
+            // 
+            // memoEdit1
+            // 
+            this.memoEdit1.Location = new System.Drawing.Point(108, 38);
+            this.memoEdit1.Name = "memoEdit1";
+            this.memoEdit1.Size = new System.Drawing.Size(524, 405);
+            this.memoEdit1.StyleController = this.layoutControl1;
+            this.memoEdit1.TabIndex = 6;
+            // 
+            // btnAnalyse
+            // 
+            this.btnAnalyse.Location = new System.Drawing.Point(519, 12);
+            this.btnAnalyse.Name = "btnAnalyse";
+            this.btnAnalyse.Size = new System.Drawing.Size(113, 22);
+            this.btnAnalyse.StyleController = this.layoutControl1;
+            this.btnAnalyse.TabIndex = 5;
+            this.btnAnalyse.Text = "Analyse";
+            this.btnAnalyse.Click += new System.EventHandler(this.btnAnalyse_Click);
+            // 
+            // tdInputUrl
+            // 
+            this.tdInputUrl.Location = new System.Drawing.Point(108, 12);
+            this.tdInputUrl.Name = "tdInputUrl";
+            this.tdInputUrl.Size = new System.Drawing.Size(407, 20);
+            this.tdInputUrl.StyleController = this.layoutControl1;
+            this.tdInputUrl.TabIndex = 4;
+            // 
+            // layoutControlGroup1
+            // 
+            this.layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.layoutControlGroup1.GroupBordersVisible = false;
+            this.layoutControlGroup1.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1,
+            this.layoutControlItem2,
+            this.layoutControlItem3});
+            this.layoutControlGroup1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlGroup1.Name = "layoutControlGroup1";
+            this.layoutControlGroup1.Size = new System.Drawing.Size(644, 455);
+            this.layoutControlGroup1.TextVisible = false;
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.tdInputUrl;
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(507, 26);
+            this.layoutControlItem1.Text = "Url:";
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(93, 13);
+            // 
+            // layoutControlItem2
+            // 
+            this.layoutControlItem2.Control = this.btnAnalyse;
+            this.layoutControlItem2.Location = new System.Drawing.Point(507, 0);
+            this.layoutControlItem2.Name = "layoutControlItem2";
+            this.layoutControlItem2.Size = new System.Drawing.Size(117, 26);
+            this.layoutControlItem2.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem2.TextVisible = false;
+            // 
+            // layoutControlItem3
+            // 
+            this.layoutControlItem3.Control = this.memoEdit1;
+            this.layoutControlItem3.Location = new System.Drawing.Point(0, 26);
+            this.layoutControlItem3.Name = "layoutControlItem3";
+            this.layoutControlItem3.Size = new System.Drawing.Size(624, 409);
+            this.layoutControlItem3.TextSize = new System.Drawing.Size(93, 13);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sprbProgress,
+            this.slblCaption});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 433);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(644, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // sprbProgress
+            // 
+            this.sprbProgress.Name = "sprbProgress";
+            this.sprbProgress.Size = new System.Drawing.Size(100, 16);
+            // 
+            // slblCaption
+            // 
+            this.slblCaption.Name = "slblCaption";
+            this.slblCaption.Size = new System.Drawing.Size(0, 17);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 455);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.layoutControl1);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "YouTube Grabber";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+            this.layoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.memoEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tdInputUrl.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
+        private DevExpress.XtraEditors.MemoEdit memoEdit1;
+        private DevExpress.XtraEditors.SimpleButton btnAnalyse;
+        private DevExpress.XtraEditors.TextEdit tdInputUrl;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem3;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar sprbProgress;
+        private System.Windows.Forms.ToolStripStatusLabel slblCaption;
+    }
+}
+
---
+++ YouTubeGrabber/MainForm.cs
@@ -0,0 +1,107 @@
+﻿using AngleSharp.Dom;
+using AngleSharp.Dom.Html;
+using AngleSharp.Parser.Html;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YouTubeGrabber
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            workerAnalyse = new BackgroundWorker();
+            workerAnalyse.WorkerReportsProgress = true;
+            workerAnalyse.DoWork += WorkerAnalyse_DoWork;
+            workerAnalyse.ProgressChanged += WorkerAnalyse_ProgressChanged;
+            html = "No data";
+        }
+
+        /// <summary>
+        /// Фоновый поток.
+        /// </summary>
+        private BackgroundWorker workerAnalyse;
+
+        /// <summary>
+        /// HTML-код страницы.
+        /// </summary>
+        private string html;
+
+        /// <summary>
+        /// Начальное время выполнения задачи.
+        /// </summary>
+        private DateTime t0;
+
+        private void WorkerAnalyse_DoWork(object sender, DoWorkEventArgs e)
+        {
+            workerAnalyse.ReportProgress(0);
+            try
+            {
+                //Uri uri = new Uri(tdInputUrl.Text);
+                //html = new WebClient().DownloadString(uri);
+
+                string path = @"YouTube-test.txt";
+
+                if (File.Exists(path))
+                    html = File.ReadAllText(path);
+                workerAnalyse.ReportProgress(1);
+
+                HtmlParser parser = new HtmlParser();
+                IHtmlDocument angle = parser.Parse(html);
+
+                var list = angle.All.Where(i => i.LocalName == "li");
+                /*foreach (IElement element in angle.QuerySelectorAll("h3.r a"))
+                    Console.WriteLine(element.GetAttribute("href"));
+                    */
+                workerAnalyse.ReportProgress(2);
+            }
+            catch (Exception ex)
+            {
+                html = string.Format("{0} - {1}", ex.Source, ex.Message);
+                workerAnalyse.ReportProgress(-1);
+            }
+        }
+
+        private void WorkerAnalyse_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (e.ProgressPercentage == 0)
+            {
+                sprbProgress.Value = 10;
+                slblCaption.Text = "Running...";
+            }
+            else if (e.ProgressPercentage == 1)
+            {
+                memoEdit1.Text = html;
+                sprbProgress.Value = 80;
+            }
+            else
+            {
+                TimeSpan diff = DateTime.Now - t0;
+                slblCaption.Text = string.Format("{0:0} ms", diff.TotalMilliseconds);
+            }
+        }
+
+        private void btnAnalyse_Click(object sender, EventArgs e)
+        {
+            if (!workerAnalyse.IsBusy)
+            {
+                t0 = DateTime.Now;
+                workerAnalyse.RunWorkerAsync();
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
